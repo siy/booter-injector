4f22f8618d16c93a21f0f0d748dbc3156f27c0b8
Sergiy Yevtushenko <Sergiy.Yevtushenko@gmail.com>
2017-06-06 08:12:56 +0200

    Switching from arrays to lists. Intermediate commit 2

--- src/main/java/io/booter/injector/core/supplier/Suppliers.java
+++ src/main/java/io/booter/injector/core/supplier/Suppliers.java
@@ -2,12 +2,14 @@ package io.booter.injector.core.supplier;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Supplier;
 
 import static io.booter.injector.core.supplier.Utils.*;
 
+//TODO: refactor suppliers to be algorithm-only. others merge with supplier factory. supplier factory should be static
 public final class Suppliers {
     private Suppliers() {
     }
@@ -75,7 +77,7 @@ public final class Suppliers {
     public static <T> Supplier<T> methodSupplier(Method method, List<Supplier<?>> parameters) {
         validateParameters(method, parameters, 1);
 
-        return () -> safeCall(buildMethodSupplier(method, parameters.toArray(new Supplier[parameters.size()])), method);
+        return () -> safeCall(buildMethodSupplier(method, parameters), method);
     }
 
     //TODO: used by tests only!!!
@@ -83,48 +85,48 @@ public final class Suppliers {
     public static <T> Supplier<T> methodSupplier(Method method, Supplier<?>[] parameters) {
         validateParameters(method, parameters, 1);
 
-        return () -> safeCall(buildMethodSupplier(method, parameters), method);
+        return () -> safeCall(buildMethodSupplier(method, Arrays.asList(parameters)), method);
     }
 
 
     private static <T> ThrowingSupplier<T> buildMethodSupplier(Method method,
-                                                               Supplier<?>[] suppliers) {
+                                                               List<Supplier<?>> suppliers) {
         switch (method.getParameterCount()) {
-            case  0: return () -> (T) method.invoke(suppliers[0].get());
+            case  0: return () -> (T) method.invoke(suppliers.get(0).get());
 
-            case  1: return () -> (T) method.invoke(suppliers[0].get(), suppliers[1].get());
+            case  1: return () -> (T) method.invoke(suppliers.get(0).get(), suppliers.get(1).get());
 
-            case  2: return () -> (T) method.invoke(suppliers[0].get(), suppliers[1].get(), suppliers[2].get());
+            case  2: return () -> (T) method.invoke(suppliers.get(0).get(), suppliers.get(1).get(), suppliers.get(2).get());
 
-            case  3: return () -> (T) method.invoke(suppliers[0].get(), suppliers[1].get(), suppliers[2].get(),
-                                                          suppliers[3].get());
+            case  3: return () -> (T) method.invoke(suppliers.get(0).get(), suppliers.get(1).get(), suppliers.get(2).get(),
+                                                          suppliers.get(3).get());
 
-            case  4: return () -> (T) method.invoke(suppliers[0].get(), suppliers[1].get(), suppliers[2].get(),
-                                                          suppliers[3].get(), suppliers[4].get());
+            case  4: return () -> (T) method.invoke(suppliers.get(0).get(), suppliers.get(1).get(), suppliers.get(2).get(),
+                                                          suppliers.get(3).get(), suppliers.get(4).get());
 
-            case  5: return () -> (T) method.invoke(suppliers[0].get(), suppliers[1].get(), suppliers[2].get(),
-                                                          suppliers[3].get(), suppliers[4].get(), suppliers[5].get());
+            case  5: return () -> (T) method.invoke(suppliers.get(0).get(), suppliers.get(1).get(), suppliers.get(2).get(),
+                                                          suppliers.get(3).get(), suppliers.get(4).get(), suppliers.get(5).get());
 
-            case  6: return () -> (T) method.invoke(suppliers[0].get(), suppliers[1].get(), suppliers[2].get(),
-                                                          suppliers[3].get(), suppliers[4].get(), suppliers[5].get(),
-                                                          suppliers[6].get());
+            case  6: return () -> (T) method.invoke(suppliers.get(0).get(), suppliers.get(1).get(), suppliers.get(2).get(),
+                                                          suppliers.get(3).get(), suppliers.get(4).get(), suppliers.get(5).get(),
+                                                          suppliers.get(6).get());
 
-            case  7: return () -> (T) method.invoke(suppliers[0].get(), suppliers[1].get(), suppliers[2].get(),
-                                                          suppliers[3].get(), suppliers[4].get(), suppliers[5].get(),
-                                                          suppliers[6].get(), suppliers[7].get());
+            case  7: return () -> (T) method.invoke(suppliers.get(0).get(), suppliers.get(1).get(), suppliers.get(2).get(),
+                                                          suppliers.get(3).get(), suppliers.get(4).get(), suppliers.get(5).get(),
+                                                          suppliers.get(6).get(), suppliers.get(7).get());
 
-            case  8: return () -> (T) method.invoke(suppliers[0].get(), suppliers[1].get(), suppliers[2].get(),
-                                                          suppliers[3].get(), suppliers[4].get(), suppliers[5].get(),
-                                                          suppliers[6].get(), suppliers[7].get(), suppliers[8].get());
+            case  8: return () -> (T) method.invoke(suppliers.get(0).get(), suppliers.get(1).get(), suppliers.get(2).get(),
+                                                          suppliers.get(3).get(), suppliers.get(4).get(), suppliers.get(5).get(),
+                                                          suppliers.get(6).get(), suppliers.get(7).get(), suppliers.get(8).get());
 
-            case  9: return () -> (T) method.invoke(suppliers[0].get(), suppliers[1].get(), suppliers[2].get(),
-                                                     suppliers[3].get(), suppliers[4].get(), suppliers[5].get(),
-                                                     suppliers[6].get(), suppliers[7].get(), suppliers[8].get(),
-                                                     suppliers[9].get());
-            case 10: return () -> (T) method.invoke(suppliers[0].get(), suppliers[1].get(), suppliers[2].get(),
-                                                     suppliers[3].get(), suppliers[4].get(), suppliers[5].get(),
-                                                     suppliers[6].get(), suppliers[7].get(), suppliers[8].get(),
-                                                     suppliers[9].get(), suppliers[10].get());
+            case  9: return () -> (T) method.invoke(suppliers.get(0).get(), suppliers.get(1).get(), suppliers.get(2).get(),
+                                                     suppliers.get(3).get(), suppliers.get(4).get(), suppliers.get(5).get(),
+                                                     suppliers.get(6).get(), suppliers.get(7).get(), suppliers.get(8).get(),
+                                                     suppliers.get(9).get());
+            case 10: return () -> (T) method.invoke(suppliers.get(0).get(), suppliers.get(1).get(), suppliers.get(2).get(),
+                                                     suppliers.get(3).get(), suppliers.get(4).get(), suppliers.get(5).get(),
+                                                     suppliers.get(6).get(), suppliers.get(7).get(), suppliers.get(8).get(),
+                                                     suppliers.get(9).get(), suppliers.get(10).get());
             default:
                 //Should not happen, limits are already checked
                 return null;
@@ -134,50 +136,50 @@ public final class Suppliers {
     public static <T> Supplier<T> constructorSupplier(Constructor<T> constructor, Supplier<?>[] parameters) {
         validateParameters(constructor, parameters, 0);
 
-        return () -> safeCall(buildConstructorSupplier(constructor, parameters), constructor);
+        return () -> safeCall(buildConstructorSupplier(constructor, Arrays.asList(parameters)), constructor);
     }
 
     public static <T> Supplier<T> constructorSupplier(Constructor<T> constructor, List<Supplier<?>> parameters) {
         validateParameters(constructor, parameters, 0);
 
-        return () -> safeCall(buildConstructorSupplier(constructor, parameters.toArray(new Supplier[parameters.size()])), constructor);
+        return () -> safeCall(buildConstructorSupplier(constructor, parameters), constructor);
     }
 
     private static <T> ThrowingSupplier<T> buildConstructorSupplier(Constructor<T> constructor,
-                                                                    Supplier<?>[] suppliers) {
+                                                                    List<Supplier<?>> suppliers) {
         switch (constructor.getParameterCount()) {
             case  0: return () -> constructor.newInstance();
-            case  1: return () -> constructor.newInstance(suppliers[0].get());
+            case  1: return () -> constructor.newInstance(suppliers.get(0).get());
 
-            case  2: return () -> constructor.newInstance(suppliers[0].get(), suppliers[1].get());
+            case  2: return () -> constructor.newInstance(suppliers.get(0).get(), suppliers.get(1).get());
 
-            case  3: return () -> constructor.newInstance(suppliers[0].get(), suppliers[1].get(), suppliers[2].get());
+            case  3: return () -> constructor.newInstance(suppliers.get(0).get(), suppliers.get(1).get(), suppliers.get(2).get());
 
-            case  4: return () -> constructor.newInstance(suppliers[0].get(), suppliers[1].get(), suppliers[2].get(),
-                                                          suppliers[3].get());
+            case  4: return () -> constructor.newInstance(suppliers.get(0).get(), suppliers.get(1).get(), suppliers.get(2).get(),
+                                                          suppliers.get(3).get());
 
-            case  5: return () -> constructor.newInstance(suppliers[0].get(), suppliers[1].get(), suppliers[2].get(),
-                                                          suppliers[3].get(), suppliers[4].get());
+            case  5: return () -> constructor.newInstance(suppliers.get(0).get(), suppliers.get(1).get(), suppliers.get(2).get(),
+                                                          suppliers.get(3).get(), suppliers.get(4).get());
 
-            case  6: return () -> constructor.newInstance(suppliers[0].get(), suppliers[1].get(), suppliers[2].get(),
-                                                          suppliers[3].get(), suppliers[4].get(), suppliers[5].get());
+            case  6: return () -> constructor.newInstance(suppliers.get(0).get(), suppliers.get(1).get(), suppliers.get(2).get(),
+                                                          suppliers.get(3).get(), suppliers.get(4).get(), suppliers.get(5).get());
 
-            case  7: return () -> constructor.newInstance(suppliers[0].get(), suppliers[1].get(), suppliers[2].get(),
-                                                          suppliers[3].get(), suppliers[4].get(), suppliers[5].get(),
-                                                          suppliers[6].get());
+            case  7: return () -> constructor.newInstance(suppliers.get(0).get(), suppliers.get(1).get(), suppliers.get(2).get(),
+                                                          suppliers.get(3).get(), suppliers.get(4).get(), suppliers.get(5).get(),
+                                                          suppliers.get(6).get());
 
-            case  8: return () -> constructor.newInstance(suppliers[0].get(), suppliers[1].get(), suppliers[2].get(),
-                                                          suppliers[3].get(), suppliers[4].get(), suppliers[5].get(),
-                                                          suppliers[6].get(), suppliers[7].get());
+            case  8: return () -> constructor.newInstance(suppliers.get(0).get(), suppliers.get(1).get(), suppliers.get(2).get(),
+                                                          suppliers.get(3).get(), suppliers.get(4).get(), suppliers.get(5).get(),
+                                                          suppliers.get(6).get(), suppliers.get(7).get());
 
-            case  9: return () -> constructor.newInstance(suppliers[0].get(), suppliers[1].get(), suppliers[2].get(),
-                                                          suppliers[3].get(), suppliers[4].get(), suppliers[5].get(),
-                                                          suppliers[6].get(), suppliers[7].get(), suppliers[8].get());
+            case  9: return () -> constructor.newInstance(suppliers.get(0).get(), suppliers.get(1).get(), suppliers.get(2).get(),
+                                                          suppliers.get(3).get(), suppliers.get(4).get(), suppliers.get(5).get(),
+                                                          suppliers.get(6).get(), suppliers.get(7).get(), suppliers.get(8).get());
 
-            case 10: return () -> constructor.newInstance(suppliers[0].get(), suppliers[1].get(), suppliers[2].get(),
-                                                          suppliers[3].get(), suppliers[4].get(), suppliers[5].get(),
-                                                          suppliers[6].get(), suppliers[7].get(), suppliers[8].get(),
-                                                          suppliers[9].get());
+            case 10: return () -> constructor.newInstance(suppliers.get(0).get(), suppliers.get(1).get(), suppliers.get(2).get(),
+                                                          suppliers.get(3).get(), suppliers.get(4).get(), suppliers.get(5).get(),
+                                                          suppliers.get(6).get(), suppliers.get(7).get(), suppliers.get(8).get(),
+                                                          suppliers.get(9).get());
             default:
                 //Should not happen, limits are already checked
                 return null;
