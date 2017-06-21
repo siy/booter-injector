46affc9f503bb14649815fef5f2682f684b19e12
Sergiy Yevtushenko <sergiy.yevtushenko@grandparade.co.uk>
2017-06-21 16:40:37 +0200

    Fixed annotation declaration

--- src/main/java/org/rxbooter/injector/annotations/Singleton.java
+++ src/main/java/org/rxbooter/injector/annotations/Singleton.java
@@ -16,10 +16,14 @@
 
 package org.rxbooter.injector.annotations;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE})
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Documented
 public @interface Singleton {
     ComputationStyle value() default ComputationStyle.LAZY;
